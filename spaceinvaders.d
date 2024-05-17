db7817d388011e9b6f171c6662a7c77527edcce5
ANTON <AntoniousEmad75@gmail.com>
2024-05-17 13:59:51 +0300

    teted_correct_on_keil

--- spaceinvaders.d
+++ spaceinvaders.d
@@ -1,5 +1,5 @@
 .\spaceinvaders.o: SpaceInvaders.c
-.\spaceinvaders.o: ..//tm4c123gh6pm.h
+.\spaceinvaders.o: ../tm4c123gh6pm11.h
 .\spaceinvaders.o: Nokia5110.h
 .\spaceinvaders.o: Random.h
 .\spaceinvaders.o: TExaS.h
